3bf8f107c9fbb99562443a736104486935525c61
Thin Ei Khaing <thineikhaing@gmail.com>
2023-09-11 01:02:31 +0800

    add new migration to delete role from user and setup workflow to build and upload image to ecr

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_05_075014) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_10_131237) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -34,7 +34,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_05_075014) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username", default: "", null: false
-    t.integer "role", default: 0, null: false
     t.string "jti", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["jti"], name: "index_users_on_jti", unique: true
